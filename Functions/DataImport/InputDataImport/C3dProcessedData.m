7e4e10041cf22c7d729056b58c4e6a28e9f572de
cpontonn <cpontonn@irisa.fr>
2019-01-30 22:00:10 +0100

    Modifications in C3D prefixes hadnling (preventing bugs with mutliples "subjects" fields in metadata)

--- Functions/DataImport/InputDataImport/C3dProcessedData.m
+++ Functions/DataImport/InputDataImport/C3dProcessedData.m
@@ -27,11 +27,11 @@ h = btkReadAcquisition([char(filename) '.c3d']);
 [markers,markersInfo]=btkGetMarkers(h);
 v=btkGetMarkersValues(h);
 
-f=markersInfo.frequency;  % frequence
+f=markersInfo.frequency;  % frequency
 
-Firstframe=btkGetFirstFrame(h); % numéro de la frame 1
-Lastframe=btkGetLastFrame(h); % numéro de la dernière frame
-nb_frame=Lastframe-Firstframe+1;  % nb de frame
+Firstframe=btkGetFirstFrame(h); % first frame
+Lastframe=btkGetLastFrame(h); % last frame
+nb_frame=Lastframe-Firstframe+1;  % frames count
 
 prefixInfo = btkGetMetaData(h);
 % prefix = [prefixInfo.children.SUBJECTS.children.NAMES.info.values{1} '_'];
@@ -42,44 +42,70 @@ real_markers=struct;
 list_m_bis=cell(1,1);
 list_m_table=cell(1,1);
 
-% On crée un tableau avec toutes les combinaisons de noms de marqueurs
-% possibles avec tous les préfixes disponibles dans le c3d.
+%prefixInfo.children.SUBJECTS_2.children.NAMES.info.values  
 % Creation of a table with all combinations of marker names 
 % with all the prefixes available in the c3d. 
-if isfield(prefixInfo.children, 'SUBJECTS')
-nb_pref=length(prefixInfo.children.SUBJECTS.children.NAMES.info.values)+1;
-else nb_pref=1;
+%testing all "subjects" fields
+
+%% old way for versioning
+% s1='SUBJECTS';
+% nb_pref=0;
+% i=2;
+% if isfield(prefixInfo.children, s1)
+%     %intégrer test "SUBJECTS_XXX"
+%     
+% nb_pref=length(prefixInfo.children.SUBJECTS.children.NAMES.info.values)+1;
+% else
+% nb_pref=1;
+% end
+    
+%% new way
+
+prefix{1} = [];%markers without prefix
+
+%finding all the subjects in which prefixes are hidden
+j=1;
+k=1;
+s1='SUBJECTS';
+cal='prefixInfo.children.%s.children.NAMES.info.values';
+if isfield(prefixInfo.children, s1)
+    i=1;
+while i<=length(prefixInfo.children.SUBJECTS.children.NAMES.info.values)
+    prefix{j+1} = [prefixInfo.children.SUBJECTS.children.NAMES.info.values{i} '_'];
+    i=i+1;
+    j=j+1;
 end
-prefix=cell(1,3);
-for i_pref=1:nb_pref
-    if i_pref==nb_pref
-        prefix{i_pref} = []; %marqueurs sans prefixe (marker without prefix)
-    else
-    prefix{i_pref} = [prefixInfo.children.SUBJECTS.children.NAMES.info.values{i_pref} '_'];
+end
+
+%% additional subjects fields
+s=strcat(s1,'_',num2str(k+1));
+while isfield(prefixInfo.children, s)
+    i=1;
+        cal2=sprintf(cal,s);
+        w=eval(cal2);
+for i=1:length(w)
+    if ~any(strcmp(prefix,[w{i} '_']))
+    prefix{j+1} = [w{i} '_'];
+    j=j+1;
     end
+end
+k=k+1;
+s=strcat(s1,'_',num2str(k+1));
+end
+
+%% constituting marker names with prefixes
+
+nb_pref=length(prefix);
+
+for i_pref=1:nb_pref
+
     for j=1:length(list_markers)
         list_m_table{i_pref,j} = list_markers{j};
         list_m_bis{i_pref,j} = strcat(prefix{i_pref},list_markers{j}); % toutes les combinaisons de noms de marqueurs possibles (all combinations of possible markers names)
     end
 end
 
-% for i_pref=1:length(prefixInfo.children.SUBJECTS.children.NAMES.info.values)
-%     prefix{i_pref} = [prefixInfo.children.SUBJECTS.children.NAMES.info.values{i_pref} '_'];
-%     for j=1:size(list_markers,1)
-%         i=1;
-%         while strcmp(ListMarkersName{i},strcat(prefix{i_pref},list_markers{j})) == 0 && strcmp(ListMarkersName{i},list_markers{j}) == 0 ... % on cherche la position du jième marqueur de la liste dans le C3d
-% Looking for position of j-iest marker of the list within C3D
-%             i=i+1;
-%         end
-%         % lorsqu'on la trouvée on créé une structure avec son nom et sa position (matrice nbframe x 3)
-%         real_markers(j).name=list_markers(j);
-%         real_markers(j).position_c3d=v(:,(3*(i-1)+1):(3*(i-1)+3))/1000;
-%     end
-% end
-
-% On cherche dans toutes les combinaisons de nom de marqueurs, lesquels
-% sont présents
-% Which markers are present within all the combination ?
+%% Which markers are present within all the combination ?
 [list_marker_c3d,Ia,~]=intersect(list_m_bis,ListMarkersName,'stable');
 
 for ii=1:length(list_marker_c3d)
@@ -93,7 +119,7 @@ if ~isempty(list_missing_markers_in_c3d)
 disp(['Markerset have not been extracted enterely from the .c3d file, missing markers : ' ;list_missing_markers_in_c3d])
 end
 
-%Création du vecteur temps
+%% creating time
 for i=1:numel(real_markers)
     real_markers(i).time=[0:(1/f):(nb_frame-1)/f]'; %#ok<NBRAK>
 end
