3c213f55cecaf96b35047017124ac8e52e3458e8
Pierre <pierre.puchaud@ens-rennes.fr>
2020-01-08 16:01:11 +0100

    Editing Interface functions is now possible in Matlab 2019

--- Functions/Animation/ViewTheModel.m
+++ Functions/Animation/ViewTheModel.m
@@ -17,19 +17,27 @@ clear all
 close all
 load('ModelParameters.mat')
 
-X=cFigure; hold on;
-XX=axes;
+X=cFigure; grid off; hold on; axis off;
+XX=axes; set(XX,'visible','off')
 AnimateParameters.Mode = 'GenerateParameters';
-AnimateParameters.seg_anim = 1;
-AnimateParameters.bone_anim = 0;
+% AnimateParameters.Mode = 'cFigure';
+AnimateParameters.seg_anim = 0;
+AnimateParameters.bone_anim = 1;
 AnimateParameters.mass_centers_anim = 0;
 AnimateParameters.Global_mass_center_anim = 0;
 AnimateParameters.muscles_anim = 1;
-AnimateParameters.mod_marker_anim = 1;
+AnimateParameters.mod_marker_anim = 0;
 AnimateParameters.exp_marker_anim = 0;
 AnimateParameters.external_forces_anim = 0;
 AnimateParameters.external_forces_pred = 0;
 AnimateParameters.PictureFrame = 1;
 AnimateParameters.ax = XX;
 PlotAnimation(ModelParameters, AnimateParameters);
-axis equal;
+
+view(2); axis equal; axis tight; axis vis3d; grid on; box on;
+% camlight left; 
+axis off; axis manual;
+lighting gouraud
+ax=gca;
+ax.Clipping = 'off';
+drawnow;
