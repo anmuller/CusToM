4a2d47577f8afe22b97f3024b51bac0e19aa652f
LivetClr <claire.livet@ens-rennes.fr>
2020-02-25 16:07:49 +0100

    Functions to calculate closed-loops
    - modification of find_solid_path to return 2 paths instead of one
    - addition of two functions to calculate at every q the closed-loop constraints

--- Functions/AlgoMathsModel/find_solid_path_ClosedLoop.m
+++ Functions/AlgoMathsModel/find_solid_path_ClosedLoop.m
@@ -1,4 +1,4 @@
-function [solid_path]=find_solid_path_ClosedLoop(Human_model,i,k)
+function [solid_1_path,solid_2_path]=find_solid_path_ClosedLoop(Human_model,i,k)
 % Identification of the hierarchical path between two solids of a closed
 % loop
 % This function should not be used for path finding of two solids that are
@@ -7,7 +7,7 @@ function [solid_path]=find_solid_path_ClosedLoop(Human_model,i,k)
 %
 %   INPUT
 %   - Human_model: osteo-articular model (see the Documentation for the
-%   structure) 
+%   structure)
 %   - i: number of the first solid
 %   - k: number of the second solid
 %   OUTPUT
@@ -21,44 +21,51 @@ function [solid_path]=find_solid_path_ClosedLoop(Human_model,i,k)
 % Authors : Antoine Muller, Charles Pontonnier, Pierre Puchaud and
 % Georges Dumont
 %________________________________________________________
-
 % Path to tree root starting from solid i
+
 solid_1_root_path = find_solid_root_path(Human_model,i);
+
 % Path to tree root starting from solid k
+
 solid_2_root_path = find_solid_root_path(Human_model,k);
 
+
 % Finding closest common solid to both root paths
+
 common_ancestors = intersect(solid_1_root_path,solid_2_root_path);
 nearest_ancestor = max(common_ancestors);
 solid_1_ancestor_ind = find(solid_1_root_path==nearest_ancestor);
 solid_2_ancestor_ind = find(solid_2_root_path==nearest_ancestor);
 
-% If the path between both solids is easy (simple line on tree) ...
-% There is no need to go into more detail
-if nearest_ancestor==k
-    solid_path = solid_1_root_path(solid_1_ancestor_ind:end);
-    
-elseif nearest_ancestor==i
-    solid_path = solid_2_root_path(solid_2_ancestor_ind:end);
-    
+
+% % If the path between both solids is easy (simple line on tree) ...
+% % There is no need to go into more detail
+% if nearest_ancestor==k
+%     solid_path = solid_1_root_path(solid_1_ancestor_ind:end);
+%
+% elseif nearest_ancestor==i
+%     solid_path = solid_2_root_path(solid_2_ancestor_ind:end);
+%
 % If the path between both solids is a lace on tree
+% else
+
+if i>k % Starting from smallest solid increment
+    solid_1_path = solid_1_root_path(solid_1_ancestor_ind:end);
+    solid_2_path = sort(solid_2_root_path(solid_2_ancestor_ind:end),'descend');
 else
-    if i>k % Starting from smallest solid increment
-        solid_1_path_to_ancestor = solid_1_root_path(solid_1_ancestor_ind:end);
-        solid_2_path_from_ancestor = sort(solid_2_root_path(solid_2_ancestor_ind+1:end),'descend');
-        solid_path = [solid_2_path_from_ancestor solid_1_path_to_ancestor];
-    else
-        solid_1_path_to_ancestor = sort(solid_1_root_path(solid_1_ancestor_ind:end),'descend');
-        solid_2_path_from_ancestor = solid_2_root_path(solid_2_ancestor_ind+1:end);
-        solid_path = [solid_1_path_to_ancestor solid_2_path_from_ancestor];
-    end  
+    solid_1_path = sort(solid_1_root_path(solid_1_ancestor_ind:end),'descend');
+    solid_2_path = solid_2_root_path(solid_2_ancestor_ind:end);
 end
+
+% end
+
 end
 
 function solid_root_path=find_solid_root_path(Human_model,i,solid_root_path)
+
 if nargin < 3   % if first incrementation
     solid_root_path=i;
-
+    
 elseif i==1
     solid_root_path = [1 solid_root_path];
     return
@@ -76,3 +83,4 @@ else
 end
 
 end
+
