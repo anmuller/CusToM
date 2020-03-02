36966320d492aafe6fa3b13b34fec4284d3a263c
Theo Rouvier <theo.rouvier@ensam.eu>
2020-03-02 15:16:23 +0100

    Second step for shoulder muscles

--- Functions/Models/Muscles/ShoulderMuscles.m
+++ Functions/Models/Muscles/ShoulderMuscles.m
@@ -43,30 +43,23 @@ s=[s;{
     [Side '_serr_ant_11'],23,0.0885,[],0.003,0,{[Side '_Scapula_serr_ant_11-P1'];[Side '_Thorax_serr_ant_11-P2']},{};...
     [Side '_serr_ant_12'],112,0.0945,[],0.001,0,{[Side '_Scapula_serr_ant_12-P1'];[Side '_Thorax_serr_ant_12-P2']},{};...
     [Side '_rhomboid_min'],51,0.0986,[],0.015,0,{[Side '_Thorax_rhomboid_min-P1'];[Side '_Scapula_rhomboid_min-P2']},{};...
-    [Side '_rhomboid_maj'],383,0.1193,[],0.0423,0,{[Side '_Thorax_rhomboid_min-P1'];[Side '_Scapula_rhomboid_min-P2']},{};...
-        
-%     [Side '_DELT1'],1218.9,0.0976,[],0.093,0.38397... EDIT
-%     [Side '_DELT2'],1103.5,0.1078,[],0.1095,0.2618... EDIT
-%     [Side '_DELT3'],201.6,0.1367,[],0.038,0.31416... EDIT
-% 
+    [Side '_rhomboid_maj'],383,0.1193,[],0.0423,0,{[Side '_Thorax_rhomboid_min-P1'];[Side '_Scapula_rhomboid_min-P2']},{};...        
+    [Side '_DELT1'],1218.9,0.0976,[],0.093,0.38397,{[Side '_humerus_r_DELT1_r-P1'];[Side '_humerus_r_DELT1_r-P1'];[Side '_Scapula_DELT1-P3'];[Side '_clavicle_r_DELT1_r-P4']},{};... 
+    [Side '_DELT2'],1103.5,0.1078,[],0.1095,0.2618,{[Side '_humerus_r_DELT2_r-P1'];[Side '_Scapula_DELT2-P3'];[Side '_Scapula_DELT2-P4']},{};... 
+    [Side '_DELT3'],201.6,0.1367,[],0.038,0.31416,{[Side '_Scapula_DELT3-P1'];[Side '_Scapula_DELT3-P2'];[Side '_humerus_r_DELT3_r-P3']},{};...
+    [Side '_PECM1'],444.3,0.1442,[],0.0028,0.29671,{[Side '_humerus_r_PECM1_r-P1'];[Side '_humerus_r_PECM1_r-P2'];[Side '_Thorax_PECM1-P3'];[Side '_clavicle_r_PECM1_r-P4']},{};...
+    [Side '_PECM2'],658.3,0.1385,[],0.089,0.45379,{[Side '_humerus_r_PECM2_r-P1'];[Side '_humerus_r_PECM2_r-P2'];[Side '_Thorax_PECM2-P3'];[Side '_Thorax_PECM2-P4'];[Side '_Thorax_PECM2-P5']},{};... 
+    [Side '_PECM3'],498.1,0.1385,[],0.132,0.43633,{[Side '_humerus_r_PECM3_r-P1'];[Side '_humerus_r_PECM3_r-P2'];[Side '_Thorax_PECM3-P3'];[Side '_Thorax_PECM3-P4'];[Side '_Thorax_PECM3-P5']},{};... 
+ 
 %     [Side '_SUPSP'],499.2,0.0682,[],0.0395,0.12217... EDIT
 %     [Side '_INFSP'],1075.8,0.0755,[],0.0308,0.33161... EDIT
 %     [Side '_SUBSC'],1306.9,0.0873,[],0.033,0.34907... EDIT
 %     [Side '_TMIN'],269.5,0.0741,[],0.0713,0.41888... EDIT
-%     [Side '_TMAJ'],144,0.1624,[],0.02,0.27925... EDIT
-%     
-%     [Side '_PECM1'],444.3,0.1442,[],0.0028,0.29671... EDIT
-%     [Side '_PECM2'],658.3,0.1385,[],0.089,0.45379... EDIT
-%     [Side '_PECM3'],498.1,0.1385,[],0.132,0.43633... EDIT
-%     
-%     
+%     [Side '_TMAJ'],144,0.1624,[],0.02,0.27925... EDIT   
 %     [Side '_LAT1'],290.5,0.254,[],0.12,0.43633... EDIT
 %     [Side '_LAT2'],317.5,0.2324,[],0.1765,0.33161... EDIT
 %     [Side '_LAT3'],189,0.2789,[],0.1403,0.36652... EDIT
 %     [Side '_CORB'],208.2,0.0932,[],0.097,0.47124... EDIT
-%     
-    
-    
 %     [Side '_TRIlong'],771.8,0.134,[],0.143,0.20944... EDIT
 %     [Side '_TRIlat'],717.5,0.1138,[],0.098,0.15708... EDIT
 %     [Side '_TRImed'],717.5,0.1138,[],0.0908,0.15708... EDIT
@@ -95,7 +88,6 @@ s=[s;{
 %     [Side '_trap_cl'],77.6,0.084,[],0.11922,... EDIT
 %     [Side '_trap_acr'],376.95,0.092,[],0.076582,... EDIT
 %     [Side '_levator_scap'],76.3,0.113,[],0.021875,... EDIT
-   
 }];
 
 % Structure generation
