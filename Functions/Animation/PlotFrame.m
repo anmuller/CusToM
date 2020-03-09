1ffa8831a1c52fb475b15caf0e2309d4f8dfd172
Pierre <pierre.puchaud@ens-rennes.fr>
2020-03-09 11:11:40 +0100

    Plot a frame of a segment

---
+++ Functions/Animation/PlotFrame.m
@@ -0,0 +1,21 @@
+function [h] = PlotFrame(P,R,scale)
+% Generation of a frame
+%
+%   INPUT
+%   - P: position vector 
+%   - R: rotation matrix
+%   - scale: scale the plotted arrows
+%________________________________________________________
+%
+% Licence
+% Toolbox distributed under 3-Clause BSD License
+%________________________________________________________
+%
+% Authors : Louise Demestre
+%________________________________________________________
+hold on
+axis equal
+h(1) = quiver3(P(1),P(2),P(3),R(1,1),R(1,2),R(1,3),scale);
+h(2) = quiver3(P(1),P(2),P(3),R(2,1),R(2,2),R(2,3),scale);
+h(3) = quiver3(P(1),P(2),P(3),R(3,1),R(3,2),R(3,3),scale);
+end
