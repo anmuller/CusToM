a9f964dee0ab2c95efb4c8329ab25b3f04693b46
Theo Rouvier <theo.rouvier@ensam.eu>
2020-03-09 13:48:16 +0100

    Computation of Lm

--- Functions/MuscleForces/Optimization/ForcesComputationOptiNum.m
+++ Functions/MuscleForces/Optimization/ForcesComputationOptiNum.m
@@ -44,12 +44,20 @@ idm = logical([Muscles.exist]);
 Nb_muscles=numel(Muscles(idm));
 
 %% computation of muscle moment arms from joint posture
-Lm=zeros(Nb_muscles,Nb_frames);
+L0=zeros(Nb_muscles,1);
+Ls=zeros(Nb_muscles,1);
+for i=1:Nb_muscles
+    L0(i) = BiomechanicalModel.Muscles(i).l0;
+    Ls(i) = BiomechanicalModel.Muscles(i).ls;
+end
+Lmt=zeros(Nb_muscles,Nb_frames);
 R=zeros(Nb_q,Nb_muscles,Nb_frames);
 for i=1:Nb_frames % for each frames
-    Lm(idm,i)   =   MuscleLengthComputationNum(BiomechanicalModel,q(:,i)); %dependant of every q (q_complete)
+    Lmt(idm,i)   =   MuscleLengthComputationNum(BiomechanicalModel,q(:,i)); %dependant of every q (q_complete)
     R(:,:,i)    =   MomentArmsComputationNum(BiomechanicalModel,q(:,i),0.0001); %depend on reduced set of q (q_red)
 end
+
+Lm = Lmt./(Ls./L0+1);
 
 [idxj,~]=find(sum(R(:,:,1),2)~=0);
 
@@ -69,8 +77,7 @@ options2 = optimoptions(@fmincon,'Algorithm','sqp','Display','final','GradObj','
 
 h = waitbar(0,['Forces Computation (' filename ')']);
 
-if isfield(BiomechanicalModel.OsteoArticularModel,'ClosedLoop')
-    
+if isfield(BiomechanicalModel.OsteoArticularModel,'ClosedLoop') && ~isempty([BiomechanicalModel.OsteoArticularModel.ClosedLoop])    
     % TO BE CHANGED AFTER CALIBRATION
     k=ones(size(q,1),1);
     
@@ -117,7 +124,7 @@ if isfield(BiomechanicalModel.OsteoArticularModel,'ClosedLoop')
     
     MuscleForcesComputationResults.MuscleActivations(idm,:) = Aopt;
     MuscleForcesComputationResults.MuscleForces(idm,:) = Fopt(1:Nb_muscles,:);
-    MuscleForcesComputationResults.MuscleLengths= Lm;
+    MuscleForcesComputationResults.MuscleLengths= Lmt;
     MuscleForcesComputationResults.MuscleLeverArm = R;
     
     
@@ -148,7 +155,7 @@ else
     
     MuscleForcesComputationResults.MuscleActivations(idm,:) = Aopt;
     MuscleForcesComputationResults.MuscleForces(idm,:) = Fopt;
-    MuscleForcesComputationResults.MuscleLengths= Lm;
+    MuscleForcesComputationResults.MuscleLengths= Lmt;
     MuscleForcesComputationResults.MuscleLeverArm = R;
     
 end
