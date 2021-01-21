e56d8c3b0aee3177b2d96f4fc00c119e0604714e
LivetClr <claire.livet@ens-rennes.fr>
2021-01-21 10:00:43 +0100

    Minor bug fix

--- Functions/MuscleForces/MomentArmsComputationNum.m
+++ Functions/MuscleForces/MomentArmsComputationNum.m
@@ -55,7 +55,7 @@ for k=1:length(row)
     % compute the length of the muscle at q-dq
     Lmdq(j) = Muscle_lengthNum(Human_model,Muscles(idxm(j)),q-dq);
 
-    R(:,i)=(-Lpdq+Lmdq)/(2*dp); % it is -dl/dq
+    R(j,i)=(-Lpdq+Lmdq)/(2*dp); % it is -dl/dq
 end
 % beware that the matrix is finally nq*nm
 R=R';
