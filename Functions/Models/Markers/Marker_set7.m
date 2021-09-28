4b4c50960be677a6b235622e31fb37ae6401e50f
TheoRouvier <theo.rouvier@ensam.eu>
2021-09-28 13:20:51 +0200

    Added new marker_set for ENSAM trials

---
+++ Functions/Models/Markers/Marker_set7.m
@@ -0,0 +1,67 @@
+function [Markers]=Marker_set7(varargin)
+% Definition of the markers set used in ENSAM
+%
+%   OUTPUT
+%   - Markers: set of markers (see the Documentation for the structure) 
+%________________________________________________________
+%
+% Licence
+% Toolbox distributed under GPL 3.0 Licence
+%________________________________________________________
+%
+% Authors : Antoine Muller, Charles Pontonnier, Pierre Puchaud and
+% Georges Dumont
+%________________________________________________________
+s=cell(0);
+
+% Trunk
+s=[s;{'TMPD' 'RFHD' {'Off';'Off';'On'}; 'TMPG' 'LFHD' {'Off';'Off';'On'}; 'OCCD' 'RBHD' {'Off';'On';'On'}; 'OCCG' 'LBHD' {'Off';'Off';'On'}; ...
+    'C7' 'C7' {'On';'On';'Off'}; 'MAN' 'CLAV' {'On';'On';'Off'}; ...
+    'ACD' 'RSHO' {'Off';'Off';'On'}; 'ACG' 'LSHO' {'Off';'Off';'On'}; ...
+    'EASD' 'RFWT' {'On';'Off';'On'}; 'EASG' 'LFWT' {'On';'Off';'On'}; 'EPSD' 'RBWT' {'On';'Off';'On'}; 'EPSG' 'LBWT' {'On';'Off';'On'}; ...
+    'T8' 'T8' {'Off';'On';'Off'}; 'T12' 'T12' {'Off';'On';'Off'}; ...
+    'CLAVD' 'CLAVD' {'On';'On';'On'}; 'CLAVG' 'CLAVG' {'On';'On';'On'}; ...
+    'SCAPDL' 'MTACDL' {'On';'On';'On'}; 'SCAPGL' 'MTACGL' {'On';'On';'On'};...
+    'SCAPDB' 'MTACDB' {'On';'On';'On'}; 'SCAPGB' 'MTACGB' {'On';'On';'On'};...
+    'SCAPDH' 'MTACDM' {'On';'On';'On'}; 'SCAPGH' 'MTACGM' {'On';'On';'On'};...
+    }];
+
+
+Side1={'D';'G'};
+Side2={'R';'L'};
+% Arm
+for i=1:2
+    s=[s;{['EL' Side1{i}] [Side2{i} 'RAD'] {'On';'On';'Off'}; ['EM' Side1{i}] [Side2{i} 'HUM'] {'Off';'Off';'Off'}; ...
+        ['PSR' Side1{i}] [Side2{i} 'WRA'] {'Off';'Off';'Off'}; ['PSU' Side1{i}] [Side2{i} 'WRB'] {'Off';'On';'Off'}; ...
+        ['MC2' Side1{i}] [Side2{i} 'CAR2'] {'Off';'Off';'Off'}; ['MC5' Side1{i}] [Side2{i} 'OHAND'] {'Off';'Off';'Off'}; ...
+        }];
+end
+
+% Leg
+for i=1:2
+    s=[s;{['CL' Side1{i}] [Side2{i} 'KNE'] {'Off';'Off';'On'}; ['CM' Side1{i}] [Side2{i} 'KNI'] {'Off';'On';'On'}; ...
+        ['ML' Side1{i}] [Side2{i} 'ANE'] {'Off';'On';'Off'}; ['MM' Side1{i}] [Side2{i} 'ANI'] {'Off';'Off';'Off'}; ...
+        ['CAL' Side1{i}] [Side2{i} 'HEE'] {'Off';'On';'Off'}; ...
+        ['MT1' Side1{i}] [Side2{i} 'TARI'] {'Off';'On';'On'}; ['MT5' Side1{i}] [Side2{i} 'TAR'] {'Off';'Off';'Off'}; ...
+        }];
+end
+
+
+if length(varargin)>1
+    Scapulalocator = varargin{2};
+    if Scapulalocator.active
+        for idx=1:2
+            if ~isempty(find(strcmp(Scapulalocator.side,Side2{idx}),1))
+                s=[s;{'SCLL' ['ScapLoc_AA_',Side2{idx}] {'On';'On';'On'};...
+                    'SCLM' ['ScapLoc_TS_',Side2{idx}] {'On';'On';'On'};...
+                    'SCLB' ['ScapLoc_AI_',Side2{idx}] {'On';'On';'On'};...
+                   }];
+            end
+        end
+    end
+end
+
+
+Markers=struct('name',{s{:,1}}','anat_position',{s{:,2}}','calib_dir',{s{:,3}}'); %#ok<CCAT1>
+
+end
