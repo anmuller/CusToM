a82d6622e48bb4dd33199391364cd8a683f55063
TheoRouvier <theo.rouvier@ensam.eu>
2022-02-24 15:30:29 +0100

    Improvement on Quaternion computation to allow for quaternion vectors

--- Functions/Calibration/Scapula/Quaternions/QuatPowerAtHome.m
+++ Functions/Calibration/Scapula/Quaternions/QuatPowerAtHome.m
@@ -2,4 +2,4 @@ function Qp=QuatPowerAtHome(Q,pow)
 % Computes the power of a quaternion
 % Scalar-first convention
 % See : Dam, Erik B., Martin Koch, Martin Lillholm. "Quaternions, Interpolation, and Animation." University of Copenhagen, København, Denmark, 1998.
-Qp=exp(pow*log(Q));
+Qp=exp(pow.*log(Q));
